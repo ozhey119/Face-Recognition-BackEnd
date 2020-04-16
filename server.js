c35fc97641c9d9bd4dfc164902e2c9cf6da2b6fe
Ozhey119 <ozhey119@gmail.com>
2020-04-16 17:47:10 +0300

    connect to db

--- server.js
+++ server.js
@@ -11,11 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());  
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://florismeininger.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 const db = knex({
     client: 'pg',
